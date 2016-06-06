b7844a1e8a8ee7d8b34216920980aaf7cb2807d9
ardupros <l.s007@gmx.de>
2016-06-06 16:13:59 +0200

    Update request.py
    Edit description

--- request.py
+++ request.py
@@ -3,7 +3,6 @@
 # It requires the 'requests'-module, which will be automatically installed, if the script is started with administrator-privileges
 # The purpose of this script is to check for new files on the StudIP-website of the university of Passau, 
 # which are then downloaded and saved in your desired destination folder
-# Written by Leon Stieber, last edited on 13/5/16
 
 
 # Overview about the cookies:
